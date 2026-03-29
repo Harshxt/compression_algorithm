769a21ced2a0a1f80aa6932965d4ba2cebd0527c
Harshit <79516676+Harshxt@users.noreply.github.com>
2026-03-29 20:04:19 +0530

    Added Navbar

---
+++ app/components/FileDropper.jsx
@@ -0,0 +1,10 @@
+'use client';
+export default function FileDropper() {
+
+
+    return (
+        <div>
+            {/* TODO */}
+        </div>
+    );
+}
---
+++ app/components/Navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+    return (
+        <nav className="bg-gray-800 p-4 m-4 rounded-lg shadow-lg ring-1 ring-white/10">
+            <ul className="flex space-x-4 font-bold">
+                <li><a href="/" className="text-white">Compressio</a></li>
+            </ul>
+        </nav>
+    );
+}
