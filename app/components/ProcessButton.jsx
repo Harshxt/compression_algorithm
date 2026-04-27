a00c3f4c9596a1b4d28d3b3a6176e0a449cf86cc
Harshit <79516676+Harshxt@users.noreply.github.com>
2026-04-27 08:50:34 +0530

    feat: add ProcessingContext and integrate with ProcessButton for processing state management

--- app/components/ProcessButton.jsx
+++ app/components/ProcessButton.jsx
@@ -1,13 +1,15 @@
 import { MdPlayCircleOutline } from "react-icons/md";
+import { useProcessing } from "../context/ProcessingContext";
 
 
 export default function ProcessButton() {
+    const {isProcessing, setIsProcessing} = useProcessing();
     return (
         <div className="flex justify-end">
 
-            <button className="flex items-center rounded-md bg-blue-600 px-5 py-2 text-sm font-semibold text-white shadow-sm transition hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 active:bg-blue-800 disabled:cursor-not-allowed disabled:opacity-60">
-                <MdPlayCircleOutline className="mr-2 items-center" />
-                Process Files
+            <button disabled={isProcessing} className="flex items-center rounded-md bg-blue-600 px-5 py-2 text-sm font-semibold text-white shadow-sm transition hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 active:bg-blue-800 disabled:cursor-not-allowed disabled:opacity-60">
+                <MdPlayCircleOutline className="mr-2 items-center" /> Process Files
+
             </button>
         </div>
     );
---
+++ app/context/ProcessingContext.jsx
@@ -0,0 +1,28 @@
+"use client";
+
+const { useContext, createContext, useState } = require("react");
+
+const ProcessingContext = createContext(undefined);
+
+
+export function ProcessingProvider({ children }) {
+    const [isProcessing, setIsProcessing] = useState(false);
+
+
+    return (
+        <ProcessingContext.Provider value={{ isProcessing, setIsProcessing }}>
+            {children}
+        </ProcessingContext.Provider>
+    )
+
+
+}
+
+export function useProcessing() {
+    const context = useContext(ProcessingContext);
+
+    if (!context) {
+        throw new Error("useProcessing must be used with ProcessingProvider");
+    }
+    return context;
+}
